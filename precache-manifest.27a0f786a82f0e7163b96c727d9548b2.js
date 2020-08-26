6875132bf6c7f4cfe09884a0aad84fa0f727c4c7
Piotr Kielak <p.kielak@outlook.com>
2020-08-27 01:52:15 +0200

    Updates

---
+++ precache-manifest.27a0f786a82f0e7163b96c727d9548b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48573e143331a4196ecefe045843aafc",
+    "url": "/oko-card-game/index.html"
+  },
+  {
+    "revision": "8d799135015898ce5ac8",
+    "url": "/oko-card-game/static/css/main.7af74738.chunk.css"
+  },
+  {
+    "revision": "43749d93b3f862638dff",
+    "url": "/oko-card-game/static/js/2.2284ca31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oko-card-game/static/js/2.2284ca31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d799135015898ce5ac8",
+    "url": "/oko-card-game/static/js/main.b2e9fd5f.chunk.js"
+  },
+  {
+    "revision": "b89ea196ff7feb06ad34",
+    "url": "/oko-card-game/static/js/runtime-main.4f8579e1.js"
+  },
+  {
+    "revision": "8c15b137ec6085e9db1362f64619ce42",
+    "url": "/oko-card-game/static/media/back.8c15b137.png"
+  }
+]);
